99106713225891cc7133c3f645eacddd03029eb5
Ahmad Omar Ahsan <ahmadomarahsan@gmail.com>
2024-05-29 14:51:56 -0600

    Needs fixing

--- inspect_test.py
+++ inspect_test.py
@@ -7,24 +7,29 @@ import torch
 from utils import get_config,  seed_everything, Retinal_Cond_Lightning
 from model_architecture import  Pretrained_LightningDDPM_monai
 from torchvision.utils import save_image
+import pandas as pd
 
-def predict_display(test_set, timesteps, module):
+def predict_display(test_set, timesteps, module, test_index_list):
     with torch.inference_mode():
         # accuracy = []
         
-        noise = torch.randn((1, module.in_channels , module.image_h, module.image_w)).to(module.device)
-        
         test_index = 0
         for test_image, test_label in tqdm(test_set):
-            class_errors = torch.empty((6,1)).to(module.device)
+            test_index += 1
+            if test_index not in test_index_list:
+                # print(test_index)
+                # print('here')
+                continue
+            class_errors = []
+            noise = torch.randn((1, module.in_channels , module.image_h, module.image_w)).to(module.device)
             classes = torch.arange(module.num_classes).to(module.device)
             output_test_images = defaultdict(list)
             output_min_error = defaultdict(list)
-            test_index += 1
+            
             test_image = torch.unsqueeze(test_image, dim=0).to(module.device)
             
-            error = torch.empty_like(class_errors)
-
+            error = torch.zeros((6))
+            
             
             for r in timesteps:
                 time = r.reshape((1,)).to(module.device).long()
@@ -36,33 +41,43 @@ def predict_display(test_set, timesteps, module):
                     output_test_images[conditions.item()].append(noisy_image)
                     error[c] = module.criterion(noise, output,reduction='none').mean(dim=(1,2,3)).view(-1, 1).to(module.device)
                 
-                class_errors = torch.concat([class_errors, error], dim=1)
+                class_errors.append(error)
+                
                 min_error = torch.argmin(error, dim=0, keepdim=False).item()
                 output_min_error[r.item()].append(min_error)
             
-            save_image(test_image,fp=f'test_images/label_{test_label}_{test_index}.png')     
-            # print(output_min_error)
+            print(class_errors)
+            class_errors = torch.concat(class_errors, dim=1)
+            print(class_errors)
+            # save_image(test_image,fp=f'test_images/label_{test_label}_{test_index}.png')     
+            print(f"For test index: {test_index} and test label: {test_label}")
+            # print(class_errors)
+            df = pd.DataFrame(class_errors.cpu().numpy())
+            df.columns = [0, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+            df.index = ["AMD", "Cataract", "DR", "Glaucoma", "Myopia", "Normal"]
+            print(df)
+            print(output_min_error)
             # mean_error_classes = torch.mean(class_errors, dim=1)
             # min_error_index = torch.argmin(mean_error_classes, dim=0, keepdim=False) 
             
             # accuracy.append((min_error_index == test_label).float())
 
-            fig, axs = plt.subplots(11, 6, figsize=(75, 75))
+            # fig, axs = plt.subplots(11, 6, figsize=(75, 75))
 
-            plt.rc('font', size=30)
-            for i, timestep in enumerate(timesteps):
-                timestep = timestep.item()
-                for condition, image in output_test_images.items(): 
-                    image = image[i].squeeze(dim=0)
-                    image = image.permute(1, 2, 0).cpu()
-                    axs[i, condition].imshow(image)
-                    axs[i, condition].set_title(f"Condition: {condition}, Timestep: {timestep}")
+            # plt.rc('font', size=30)
+            # for i, timestep in enumerate(timesteps):
+            #     timestep = timestep.item()
+            #     for condition, image in output_test_images.items(): 
+            #         image = image[i].squeeze(dim=0)
+            #         image = image.permute(1, 2, 0).cpu()
+            #         axs[i, condition].imshow(image)
+            #         axs[i, condition].set_title(f"Condition: {condition}, Timestep: {timestep}")
                 
 
-                    if condition == 5:
-                        axs[i, condition].text(1.5, 1, f"Actual class: {test_label}, Predicted class: {output_min_error[timestep][0]}", transform=axs[i, condition].transAxes)
+            #         if condition == 5:
+            #             axs[i, condition].text(1.5, 1, f"Actual class: {test_label}, Predicted class: {output_min_error[timestep][0]}", transform=axs[i, condition].transAxes)
 
-            fig.savefig(f'subplots/condition_{test_label}_test_{test_index}_image.png')
+            # fig.savefig(f'subplots/condition_{test_label}_test_{test_index}_image.png')
 
 
 
@@ -111,12 +126,12 @@ def main():
             count_normal += 1
     
 
-    predict_display(amd, timesteps=t, module=module_lightning)
-    predict_display(cataract, timesteps=t, module=module_lightning)
-    predict_display(dr, timesteps=t, module=module_lightning)
-    predict_display(glaucoma, timesteps=t, module=module_lightning)
-    predict_display(myopia, timesteps=t, module=module_lightning)
-    predict_display(normal, timesteps=t, module=module_lightning)
+    predict_display(amd, timesteps=t, module=module_lightning, test_index_list=[2,3])
+    predict_display(cataract, timesteps=t, module=module_lightning, test_index_list=[6])
+    predict_display(dr, timesteps=t, module=module_lightning, test_index_list=[3])
+    predict_display(glaucoma, timesteps=t, module=module_lightning, test_index_list=[2])
+    predict_display(myopia, timesteps=t, module=module_lightning, test_index_list=[7])
+    predict_display(normal, timesteps=t, module=module_lightning, test_index_list=[4])
 
 
 
