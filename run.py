b41b7ffd9f7cc9d0486782612e6730c0698cd09f
Ahmad Omar Ahsan <ahmadomarahsan@gmail.com>
2025-01-18 18:52:48 -0700

    Update run.py

--- run.py
+++ run.py
@@ -43,6 +43,34 @@ def pipeline(config):
         DDPM_lightning = LightningDDPM_monai(config=config)
         trainer.fit(model=DDPM_lightning, datamodule=dm)
 
+    elif config['exp']['training_type'] == 'continue_UKBB':
+        checkpoint_callback = ModelCheckpoint(dirpath=os.path.join(config['exp']['ckpt_dir']),
+                                              monitor='val_loss',
+                                              verbose=False,
+                                              save_last=True,
+                                              save_top_k=config['exp']['save_top_k'],
+                                              save_weights_only=False,
+                                              mode='min',
+                                              filename="diffusion-{epoch:02d}-{step}-{val_loss:.5f}"
+                                              )
+        trainer = pl.Trainer(
+            logger=logger,
+            log_every_n_steps=config['exp']['log_every_n_steps'],
+            devices=config['exp']['device'],
+            min_epochs = config['hparams']['min_epochs'],
+            max_epochs = config['hparams']['max_epochs'],
+            num_sanity_val_steps=config['hparams']['num_sanity_val_steps'],
+            accelerator=config['exp']['accelerator'],
+            callbacks=[checkpoint_callback, lr_callback],
+            precision='16-mixed',
+            # profiler='pytorch',
+            accumulate_grad_batches=8
+        )
+        dm = UK_biobank_data_module(
+            config=config
+        )
+        DDPM_lightning = LightningDDPM_monai.load_from_checkpoint(config['exp']['model_ckpt_path'], strict=False, config=config)
+        trainer.fit(model=DDPM_lightning, datamodule=dm)
     elif config['exp']['training_type'] == 'scratch_conditional':
         checkpoint_callback = ModelCheckpoint(dirpath=os.path.join(config['exp']['ckpt_dir']),
                                               monitor='val_loss',
